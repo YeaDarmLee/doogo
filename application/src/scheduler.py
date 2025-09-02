1965f07a7f49f25d25d75d6dc05a7cd6f7f0971f
YeaDarmLee <gnswpwhrqkf3@gmail.com>
2025-09-03 05:19:39 +0900

    스케줄러 오류 수정

--- application/src/scheduler.py
+++ application/src/scheduler.py
@@ -1,41 +1,53 @@
+# scheduler_boot.py
+import os, atexit, logging
 from apscheduler.schedulers.background import BackgroundScheduler
 from pytz import timezone
 
+_scheduler = None  # 중복 시작 방지
+
 def start_scheduler(app):
-  """
-  Flask 앱과 함께 백그라운드 스케줄러를 기동.
-  """
+  global _scheduler
+  if _scheduler and _scheduler.running:
+    return _scheduler
+
+  # 디버그 리로더 부모 프로세스에서는 시작하지 않음
+  if app.debug and os.environ.get("WERKZEUG_RUN_MAIN") != "true":
+    return None
+
   tz = timezone('Asia/Seoul')
   scheduler = BackgroundScheduler(timezone=tz)
 
-  # 잡 함수는 app_context 내에서 실행되도록 래핑
+  # 로깅(동작 확인용)
+  aps_log = logging.getLogger("apscheduler")
+  if not aps_log.handlers:
+    aps_log.setLevel(logging.INFO)
+    aps_log.addHandler(logging.StreamHandler())
+
   from application.jobs.supplierJobs import process_pending_suppliers
 
   def job_wrapper():
     with app.app_context():
-      process_pending_suppliers(batch_size=10)
+      app.logger.info("[supplier_slack_job] started")
+      try:
+        process_pending_suppliers(batch_size=10)
+      except Exception as e:
+        app.logger.exception(e)
+      finally:
+        app.logger.info("[supplier_slack_job] finished")
 
-  # 예: 30초마다 실행, 중복 인스턴스 방지(잡 레벨) + 누락 분 합침
   scheduler.add_job(
     job_wrapper,
-    'interval',
-    seconds=5,
+    trigger='interval',
+    seconds=30,
     id='supplier_slack_job',
     max_instances=1,
     coalesce=True,
-    misfire_grace_time=60
+    misfire_grace_time=60,
+    replace_existing=True  # 리로드 시 기존 id 교체
   )
 
   scheduler.start()
-
-  # 앱 종료 시 스케줄러도 종료
-  @app.teardown_appcontext
-  def _shutdown(exception=None):
-    try:
-      if scheduler.running:
-        scheduler.shutdown(wait=False)
-    except Exception:
-      pass
-
-  # 외부에서 참조 가능하면 좋을 때 반환
+  atexit.register(lambda: scheduler.shutdown(wait=False))  # 프로세스 종료 시에만 정리
+  _scheduler = scheduler
+  app.logger.info("APScheduler started")
   return scheduler
