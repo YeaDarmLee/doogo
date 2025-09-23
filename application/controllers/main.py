f61dc5144577ab8f4e31e4966fd0420d0ed50280
YeaDarmLee <gnswpwhrqkf3@gmail.com>
2025-09-24 05:09:07 +0900

    Update main.py

--- application/controllers/main.py
+++ application/controllers/main.py
@@ -1,13 +1,14 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect, url_for
 from oauth2client.service_account import ServiceAccountCredentials
 import os, gspread
 
 main = Blueprint("main", __name__, url_prefix="/")
 
 # main 페이지 이동
-# @main.route("/")
-# def index():
+@main.route("/")
+def index():
 #   return render_template(
 #     'index.html',
 #     pageName='main'
 #   )
+  return redirect(url_for("supplier.index"))  # main 블루프린트 안에 home 함수로 이동
